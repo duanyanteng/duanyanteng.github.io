a74932d26f984867d15b0ce00dccaeb92a9d28ab
段燕腾 <duanyanteng@xianglin.cn>
2024-04-23 16:56:30 +0800

    Site updated: 2024-04-23 16:56:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/23/代码块学习/"];function toRandomPost(){
+var posts=["2024/04/23/Java开发手册/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
