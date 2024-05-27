6a636c1e25c2e7e2689ff4109c15f78dc90ec1e8
段燕腾 <duanyanteng@xianglin.cn>
2024-05-27 16:15:03 +0800

    Site updated: 2024-05-27 16:15:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/23/Java开发手册/","2024/05/06/代码学习/","2024/05/06/Java-Classfinal-加密Jar/"];function toRandomPost(){
+var posts=["2024/04/23/Java开发手册/","2024/05/06/代码学习/","2024/05/06/Java-Classfinal-加密Jar/","2024/05/27/Git-使用方法及规范/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
